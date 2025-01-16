6be1095607b7b381e3a8f806daf9a810c3db1656
Mazen Abdelmonem <mazen.abdelmonem.ibrahim@gmail.com>
2025-01-16 21:21:26 +0200

    add IEmployeeRepository.cs

---
+++ HR_Management/HR_Management.API/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using HR_Management.API.Models.Domin;
+
+namespace HR_Management.API.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<Employee> AddEmployee(Employee employee);
+        Task<Employee> UpdateEmployee(Employee employee);
+        Task<Employee> DeleteEmployee(Employee employee);
+        Task<Employee> GetById(int id);
+        Task<Employee> GetAll();
+
+    }
+}
--- HR_Management/HR_Management.API/obj/Debug/net8.0/HR_Management.API.AssemblyInfo.cs
+++ HR_Management/HR_Management.API/obj/Debug/net8.0/HR_Management.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR_Management.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b3526148f54de7e8c792d00573f5b98940968f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aa3ed68dd0f86d8b9966ecd23e4614628e2a72f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR_Management.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR_Management.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
