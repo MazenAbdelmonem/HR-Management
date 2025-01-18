6ad9d6d0e1ace52d16ad9e32724703a97b2d4fed
Mazen Abdelmonem <mazen.abdelmonem.ibrahim@gmail.com>
2025-01-18 17:23:13 +0200

    add UpdateEmployeeRequestDto.cs

---
+++ HR_Management/HR_Management.API/Models/DTO/UpdateEmployeeRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HR_Management.API.Models.DTO
+{
+    public class UpdateEmployeeRequestDto
+    {
+        public string Name { get; set; } // Employee Name
+        public string Role { get; set; }   // Job Title
+        public string Department { get; set; }
+    }
+}
