f089e2a2c37f9a1a407930e71e509fd3b314c825
Mazen Abdelmonem <mazen.abdelmonem.ibrahim@gmail.com>
2025-01-24 11:08:33 +0200

    Add AddAttendanceRequestDto.cs and AttendanceDto.cs

---
+++ HR_Management/HR_Management.API/Models/DTO/AddAttendanceRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace HR_Management.API.Models.DTO
+{
+    public class AddAttendanceRequestDto
+    {
+        public int EmployeeId { get; set; }
+        public DateTime Date { get; set; } // Date of attendance
+        public DateTime? CheckInTime { get; set; } // Entry time
+        public DateTime? CheckOutTime { get; set; } // Check out time
+        public bool IsAbsent { get; set; }
+        public double? WorkingHours { get; set; }
+    }
+}
---
+++ HR_Management/HR_Management.API/Models/DTO/AttendanceDto.cs
@@ -0,0 +1,13 @@
+﻿namespace HR_Management.API.Models.DTO
+{
+    public class AttendanceDto
+    {
+        public int Id { get; set; }
+        //public int EmployeeId { get; set; }
+        public DateTime Date { get; set; } // Date of attendance
+        public DateTime? CheckInTime { get; set; } // Entry time
+        public DateTime? CheckOutTime { get; set; } // Check out time
+        public bool IsAbsent { get; set; }
+        public double WorkingHours { get; set; }
+    }
+}
