9c72c698898bbe1ca9391924c19ddc59b13236da
Mazen Abdelmonem <mazen.abdelmonem.ibrahim@gmail.com>
2025-01-25 16:24:07 +0200

    Add ILeaveRepository.cs

---
+++ HR_Management/HR_Management.API/Repositories/ILeaveRepository.cs
@@ -0,0 +1,13 @@
+﻿using HR_Management.API.Models.Domin;
+
+namespace HR_Management.API.Repositories
+{
+    public interface ILeaveRepository
+    {
+        Task<Leave> CreatLeaveAsync(Leave leave);
+        Task<Leave>? GetLeaveByIdAsync(int id);
+        Task<Leave> GetAllAsync();
+        Task<Leave>? GUpdateLeaveAsync(int id, Leave leave);
+        Task<Leave>? DeleteLeaveAsync(int id);
+    }
+}
