7325f4fbb8d2ee407bd1980f497ea61b9366f59b
Mazen Abdelmonem <mazen.abdelmonem.ibrahim@gmail.com>
2025-01-26 12:56:22 +0200

    Add AddLeaveRequestDto.cs, LeaveDto.cs and UpdateLeaveReqeustDto.cs

---
+++ HR_Management/HR_Management.API/Models/DTO/LeaveDto.cs
@@ -0,0 +1,17 @@
+﻿using HR_Management.API.Models.Domin;
+
+namespace HR_Management.API.Models.DTO
+{
+    public class LeaveDto
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime StartDate { get; set; } // Leave start date
+        public DateTime EndDate { get; set; } // Leave end date
+        public string LeaveType { get; set; } // Type of leave (annual, sick, etc.)
+        public string Status { get; set; } // Leave status (approved, denied, etc.)
+
+        // Navigation Properties
+        public Employee Employee { get; set; }
+    }
+}
---
+++ HR_Management/HR_Management.API/Models/DTO/UpdateLeaveReqeustDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HR_Management.API.Models.DTO
+{
+    public class UpdateLeaveReqeustDto
+    {
+        public int EmployeeId { get; set; }
+        public DateTime StartDate { get; set; } // Leave start date
+        public DateTime EndDate { get; set; } // Leave end date
+        public string LeaveType { get; set; } // Type of leave (annual, sick, etc.)
+        public string Status { get; set; } // Leave status (approved, denied, etc.)
+    }
+}
