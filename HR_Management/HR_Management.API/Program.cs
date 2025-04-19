d2141889f1a56e0ebf4ddc09837e2f2337cafe90
Mazen Abdelmonem <mazen.abdelmonem.ibrahim@gmail.com>
2025-04-19 17:01:04 +0200

    add JWT

--- HR_Management/HR_Management.API/Program.cs
+++ HR_Management/HR_Management.API/Program.cs
@@ -1,6 +1,9 @@
+using System.Text;
 using HR_Management.API.Data;
 using HR_Management.API.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +18,22 @@ builder.Services.AddEndpointsApiExplorer();
 
 
 builder.Services.AddDbContext<HRManagementDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("HRManagementConnectionString")));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(option => option.TokenValidationParameters = new TokenValidationParameters
+{
+    //ValidateIssuer = true,
+    //ValidateAudience = true,
+    //ValidateIssuerSigningKey = true,
+    //ValidIssuer = builder.Configuration["Jwt:Issuer"],
+    //ValidAudience = builder.Configuration["Jwt:Audience"],
+    //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+
+    ValidateIssuer = true,
+    ValidateAudience = true,
+    ValidateIssuerSigningKey = true,
+    ValidIssuer = builder.Configuration["JWT:Issuer"],
+    ValidAudience = builder.Configuration["JWT:Audience"],
+    IssuerSigningKey  = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+});
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
